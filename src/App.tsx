eff8ee0e90458b5621916d63690418fc1f7af9c4
Antonij Djajkoski <48217266+adzajko@users.noreply.github.com>
2020-08-22 23:07:17 +0200

    Init dependencies added (#1)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import { Test } from './interfaces/Test';
+
+function App() {
+  const array: Test[] = [{ id: 0, isChecked: false, name: 'lmao' }];
+  return (
+    <div className='App'>
+      <h1>Coming Soon...</h1>
+    </div>
+  );
+}
+
+export default App;
