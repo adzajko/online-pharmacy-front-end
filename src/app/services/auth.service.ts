ce77670b840655dd2012ac48f852e057d9c7f082
AlamoFiteCat <32655601+AlamoFiteCat@users.noreply.github.com>
2020-09-15 23:42:05 +0200

    firebase authentication service established and connected to the firebase platform (#3)

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Subject, from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private afAuth: AngularFireAuth) {}
+
+  signIn(email: string, password: string) {
+    return from(this.afAuth.signInWithEmailAndPassword(email, password));
+  }
+  signOut() {
+    return from(this.afAuth.signOut());
+  }
+
+  signUp(email: string, password: string) {
+    return from(this.afAuth.createUserWithEmailAndPassword(email, password));
+  }
+
+  async sendConfirmationEmail() {
+    (await this.afAuth.currentUser).sendEmailVerification();
+  }
+
+  resetPassword(email: string) {
+    return from(
+      this.afAuth
+        .sendPasswordResetEmail(email)
+        .then(() => {
+          console.log('Password sucessfully changed.');
+        })
+        .catch((err) => {
+          console.log(err.message);
+        })
+    );
+  }
+
+  // Get the User
+  getUsername() {
+    return from(this.afAuth.currentUser);
+  }
+}
